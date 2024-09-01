b89277fe70809981615386a5381e73a786401ef3
Hamd <ajmelpareeth@gmail.com>
2024-09-01 21:19:01 +0530

    ARRAY-EXAMPLE

---
+++ triangle1.js
@@ -0,0 +1,33 @@
+var a=" "
+for(i=1;i<=5;i++){
+    
+    if(i%2==0){
+
+        a=a+" * "
+    }
+    else{
+        a=a+" $ "
+    }
+    
+    console.log(a)
+}
+
+// basics
+
+a=10
+b=20
+c=a+b
+console.log(c)
+
+// first programme
+
+a="10"  //string variable
+b=20
+c=a+b
+console.log(c)
+
+// second programme
+
+names=[273,'ajmel',9447821924,35.6,'true']
+console.log(names[1])
+console.log(names[2]) 
