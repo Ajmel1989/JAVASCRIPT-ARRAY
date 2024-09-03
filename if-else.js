4a7f000e2e452e8b354ccb730cd16b950bb65b48
Hamd <ajmelpareeth@gmail.com>
2024-09-03 21:59:40 +0530

    IF-ELSE

---
+++ if-else.js
@@ -0,0 +1,36 @@
+if(10>2){                    //syntax: if boolean value is true then print output 
+    console.log("is true")          //  :if boolean value is false then print otput else section
+}
+else{
+    console.log(" is false")
+}
+
+
+// second programe if -else- else if examples
+
+var mark= 89
+
+if(mark>=90){
+    console.log("the grade is A ")
+}
+else if(mark>=80){
+    console.log("the grade is B")
+}
+else if(mark>=70){
+    console.log("the grade is c")
+}
+else if(mark>=60){
+    console.log("the grade is d")
+}
+else if(mark>=50){
+    console.log("the grade is e")
+}
+else if(mark>=40){
+    console.log("the grade is f")
+}
+else if(mark>=30){
+    console.log("the grade is g")
+}
+else{
+    console.log("exam result is failed")
+}
