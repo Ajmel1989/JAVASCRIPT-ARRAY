39b97adb66f82b47401c282b1288ee2ddbdcba32
Hamd <ajmelpareeth@gmail.com>
2024-09-04 22:29:00 +0530

    SWITCH

---
+++ switch.js
@@ -0,0 +1,15 @@
+var fruit="litchi";
+switch(fruit){
+    case "grape":console.log("it is grape")
+    break;
+    case "banana":console.log("it is banana")
+    break;
+    case "apple":console.log("it is apple")
+    break;
+    case "mango":console.log("it is mango")
+    break;
+    case "orange":console.log("it is orange")
+    break;
+default:console.log("None of this fruit")
+
+}
