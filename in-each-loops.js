904a98702a9d3c304364e6075c1d32b52f6a9d0c
Hamd <ajmelpareeth@gmail.com>
2024-09-08 21:48:43 +0530

    IN-EACH-LOOPS

---
+++ in-each-loops.js
@@ -0,0 +1,18 @@
+ar=[10,20,30,40,50,60,70,80,90]
+person=[{"name":"ajmel"},{"name":"aliya"},{"name":"appu"},{"name":"anu"}]
+person.forEach(function(value,index){
+    console.log(value.name)
+});
+
+sum=0;
+for(i in ar ){
+    sum=sum+ar[i]
+}
+console.log("Total="+sum)
+
+total=0;
+ar.forEach(function(value,index){
+   total=total+value
+   
+})
+console.log(total) 
