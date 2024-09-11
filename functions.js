77f2a4c6f64df7b4063c46a0edf79275e2d35d1e
Hamd <ajmelpareeth@gmail.com>
2024-09-11 23:54:40 +0530

    FUNCTIONS

--- functions.js
+++ functions.js
@@ -27,5 +27,41 @@ console.log(sum(10,30));
 
 // ===================================================
 
+var s=function(a,b){
+    return a+b;
+
+}
+function abc(x){
+    console.log(x(30,40))
+}
+abc(s)
 
 
+// ========================================================
+
+function xyz(r,s){
+    console.log(r(40,50))
+    console.log(s(40,50))
+
+}
+xyz(function(e,f){
+    return e+f;
+},function(e,f){
+    return e*f;
+})
+
+
+
+// ====================================
+
+
+function ar(){
+    pr();
+    console.log("this is ar function")
+
+}
+function pr(){
+    console.log("this is pr function")
+    
+}
+ar();
