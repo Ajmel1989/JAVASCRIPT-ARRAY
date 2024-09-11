b085cfa171c637b16bd620a1077674dff6d5dd6b
Hamd <ajmelpareeth@gmail.com>
2024-09-11 23:30:49 +0530

    FUNCTIONS

---
+++ functions.js
@@ -0,0 +1,31 @@
+// simple programe===============
+
+function printline(){
+console.log("===============================")
+
+}
+
+printline();
+console.log("welcome to javascript.")
+printline();
+console.log("hello world")
+printline();
+
+
+// ==============================================
+
+function total(a,b){
+    console.log(a+b);
+}
+total(10,20);
+
+
+function sum(c,d){
+    return c+d;
+}
+console.log(sum(10,30));
+
+// ===================================================
+
+
+
