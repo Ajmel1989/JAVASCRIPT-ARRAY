1e8171b0a2c7d33a18c1bd14e5ea11c94b59e6bd
Hamd <ajmelpareeth@gmail.com>
2024-09-12 23:10:11 +0530

    PATTERNS

--- sample.js
+++ sample.js
@@ -1,6 +1,95 @@
-for(i=1; i<=100; i++){
-    if(i%2==0){
-        console.log(i)
+for(i=1;i<=5;i++){
+    a=""
+    for(j=1;j<=5;j++){
+      if(j==3||i==3){
+        a=a+"   "
+      }
+      else{
+         a=a+" * "
+      }
     }
+   console.log(a);
+}
+console.log("===============")
+for(i=1;i<=5;i++){
+    a=""
+    for(j=1;j<=5;j++){
+      if(j==3||i==3){
+        a=a+" * "
+      }
+      else{
+         a=a+"   "
+      }
+    }
+   console.log(a);
+}
+console.log("===============================")
+
+for(i=1;i<=6;i++){
+    a=""
+    for(j=5;j>=i;j--){
+        a=a+"   "
+    }
+    for(k=1;k<=i-2;k++){
+        a=a+" * "
+    }
+    for(t=1;t<=i-1;t++){
+        a=a+" * "
+    }
+    for(n=5;n>=i;n--){
+        a=a+"   "
+    }
+    console.log(a);
+}
+for(i=1;i<=5;i++){
+    a=""
+    for(j=1;j<=i;j++){
+        a=a+"   "
+    }
+    for(k=3;k>=i-1;k--){
+        a=a+" * "
+    }
+    for(t=3;t>=i;t--){
+        a=a+" * "
+    }
+    for(n=1;n<=i;n++){
+        a=a+"   "
+    }
+    console.log(a);
+    
+}
+
+console.log("===============================")
+for(i=1;i<=5;i++){
+    a=""
+    for(j=5;j>=i;j--){
+        a=a+" * "
+    }
+    for(k=1;k<=i-2;k++){
+        a=a+"   "
+    }
+    for(t=1;t<=i-1;t++){
+        a=a+"   "
+    }
+    for(n=5;n>=i;n--){
+        a=a+" * "
+    }
+    console.log(a);
+}
+for(i=1;i<=5;i++){
+    a=""
+    for(j=1;j<=i;j++){
+        a=a+" * "
+    }
+    for(k=3;k>=i-1;k--){
+        a=a+"   "
+    }
+    for(t=3;t>=i;t--){
+        a=a+"   "
+    }
+    for(n=1;n<=i;n++){
+        a=a+" * "
+    }
+    console.log(a);
     
 }
